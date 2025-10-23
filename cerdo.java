acd965e6a08cfedad936a5779a7b01742ee27ea9
Nerea <estudios26nere@gmail.com>
2025-10-23 14:27:45 +0200

    Update cerdo.java

--- cerdo.java
+++ cerdo.java
@@ -1,28 +1,28 @@
 public class Cerdo
   {
-    //atributos
-String raza="iberico";
-    boolean mordedor=false;
-    int edad=0;
-    //metodos
-public void comer()
-    {
-      System.out.println("Estoy comiendo");
-    }
-    public void morder ()
-    {
-          if(morder ==true)
+          //atributos
+      private String raza="iberico";
+      private  boolean mordedor=false;
+      private int edad=0;
+          //metodos
+      public void comer()
           {
-            System.out.println("que te muerdo")
+            System.out.println("Estoy comiendo");
           }
-          else
-          {
-            System.out.println("soy un cerdo pacifico")
-          }
-    }
-    //parametro ()
-    public int sumar(int sumando1, int sumando2)
-    {
-      return sumando1 + sumando2;
+        public void morder ()
+        {
+              if(morder ==true)
+              {
+                System.out.println("que te muerdo");
+              }
+              else
+              {
+                System.out.println("soy un cerdo pacifico");
+              }
+        }
+        //parametro ()
+        public int sumar(int sumando1, int sumando2)
+        {
+          return sumando1 + sumando2;
   }
     
