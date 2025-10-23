d67bd2ac60bfb89f50fa32ffc1684f44c08a1e16
Nerea <estudios26nere@gmail.com>
2025-10-23 14:24:04 +0200

    Create Cerdo class with attributes and methods

---
+++ cerdo.java
@@ -0,0 +1,24 @@
+public class Cerdo
+  {
+    //atributos
+String raza="iberico";
+    boolean mordedor=false;
+    int edad=0;
+    //metodos
+public void comer()
+    {
+      System.out.println("Estoy mordiendo");
+    }
+    public void morder ()
+    {
+      if(morder ==true)
+      {
+        System.out.println("que te muerdo")
+          }
+      else
+      {
+        System.out.println("soy un cerdo pacifico")
+          }
+    }
+  }
+    
